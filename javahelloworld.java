9d8aa39c85c96f8c4b5ba4a89456df57a1758403
Mayura Boosa <Mayura.Boosa@espn.com>
2015-11-02 20:57:57 -0500

    changed to String

--- javahelloworld.java
+++ javahelloworld.java
@@ -1,6 +1,6 @@
 public class javahelloworld
 {
-	public static void main (string [] args)
+	public static void main (String [] args)
 	{
 		System.out.println("Hellooo java World");
 		System.out.println("Hellooooo again");
